9f3c6b42f52a5efca3635cd42b25cea3423c1361
emmeiwhite <emmeiwhite@gmail.com>
2026-02-25 01:08:24 +0530

    chore: RateLimitedUI component - UI only --- wip

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
+import { useState } from 'react'
 import Navbar from '../components/Navbar'
+import RateLimitedUI from '../components/RateLimitedUI'
 
 export default function HomePage() {
+  const [isRateLimitReached] = useState(true)
   return (
     <div className="min-h-screen">
       <Navbar />
+      {isRateLimitReached && <RateLimitedUI />}
     </div>
   )
 }
