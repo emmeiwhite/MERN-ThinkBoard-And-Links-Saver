dcde54a5a45531f15732acb16d3c3388ddbc825a
emmeiwhite <emmeiwhite@gmail.com>
2026-02-24 17:41:15 +0530

    chore: toaster test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import { Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage'
 import EditNote from './pages/NoteDetailPage'
 import CreateNote from './pages/CreatePage'
+import toast from 'react-hot-toast'
 
 export default function App() {
   return (
     <div>
+      <button onClick={() => toast.success('Way and back again!')}>Test Toaster</button>
       <Routes>
         <Route
           path="/"
