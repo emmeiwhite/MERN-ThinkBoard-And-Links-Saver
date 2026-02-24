8c0f9c779e77616e5af5011b5585f702a3fb75b6
emmeiwhite <emmeiwhite@gmail.com>
2026-02-24 15:00:51 +0530

    chore: routing done --- matched urls to page components (basic declarative routing - react-router)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
+import { Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage'
+import EditNote from './pages/NoteDetailPage'
+import CreateNote from './pages/CreatePage'
+
 export default function App() {
   return (
     <div>
-      <h1>The Revival</h1>
-      <p>
-        Life does come up with unexpected scenarios but human being is very made special by the
-        Almighty and he/she holds the keys to unlock the closed doors provided he/she stays patient
-        and takes steps within his/her reach and never be hopeless from the God's Mercy and think
-        positive of God's plan regarding him/her.
-      </p>
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+        <Route
+          path="/note/:id"
+          element={<EditNote />}
+        />
+        <Route
+          path="/create"
+          element={<CreateNote />}
+        />
+      </Routes>
     </div>
   )
 }
