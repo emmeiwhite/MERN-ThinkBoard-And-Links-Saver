73589db4eb95a25d3e60f99ef515588f4b64f30d
emmeiwhite <emmeiwhite@gmail.com>
2026-02-24 19:36:55 +0530

    chore: forest theme enabled with daisyui plugin setting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,24 +2,11 @@ import { Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage'
 import EditNote from './pages/NoteDetailPage'
 import CreateNote from './pages/CreatePage'
-import toast from 'react-hot-toast'
+// import toast from 'react-hot-toast'
 
 export default function App() {
   return (
-    <div>
-      <h1>Tailwind CSS classes vs DaisyUI single class</h1>
-      <button
-        class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900"
-        onClick={() => toast.success('Way and back again!')}>
-        Button
-      </button>
-
-      <button
-        class="btn"
-        onClick={() => toast.success('Way and back again!')}>
-        Button
-      </button>
-
+    <div data-theme="forest">
       <Routes>
         <Route
           path="/"
