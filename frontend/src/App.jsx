5e17c776fdcf715e280fad503c1bf4c654ec017a
emmeiwhite <emmeiwhite@gmail.com>
2026-02-24 13:06:46 +0530

    chore: Frontend setup React --- boilerplate

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <div>
+      <h1>The Revival</h1>
+      <p>
+        Life does come up with unexpected scenarios but human being is very made special by the
+        Almighty and he/she holds the keys to unlock the closed doors provided he/she stays patient
+        and takes steps within his/her reach and never be hopeless from the God's Mercy and think
+        positive of God's plan regarding him/her.
+      </p>
+    </div>
+  )
+}
