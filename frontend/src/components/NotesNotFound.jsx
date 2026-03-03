5993f55edc4e1a386de1814743ca81be7bef5340
emmeiwhite <emmeiwhite@gmail.com>
2026-03-03 16:58:44 +0530

    chore: NotesNotFound Component

--- frontend/src/components/NotesNotFound.jsx
+++ frontend/src/components/NotesNotFound.jsx
@@ -4,12 +4,18 @@ import { Link } from 'react-router'
 export default function NoteNotFound() {
   return (
     <div className="flex flex-col items-center justify-center py-16 space-y-6 max-w-md mx-auto text-center">
-      <div>
+      <div className="bg-primary/10 rounded-full p-8">
         <NotebookIcon className="size-10 text-primary" />
       </div>
-      <h3>No notes yet</h3>
-      <p>Ready to organise your thoughts? Create your first note to get started on your journey.</p>
-      <Link to={'/create'}>Create Your First Note</Link>
+      <h3 className="text-2xl font-bold">No notes yet</h3>
+      <p className="text-base-content/70">
+        Ready to organise your thoughts? Create your first note to get started on your journey.
+      </p>
+      <Link
+        to={'/create'}
+        className="btn btn-primary">
+        Create Your First Note
+      </Link>
     </div>
   )
 }
