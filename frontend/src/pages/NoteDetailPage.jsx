b3cd3ca691e6f303f4b58ad862caee8c96e67888
emmeiwhite <emmeiwhite@gmail.com>
2026-03-03 20:23:46 +0530

    chore: NoteDetail Heading

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router'
 import api from '../lib/axios'
-import { LoaderIcon } from 'lucide-react'
+import { ArrowLeftIcon, LoaderIcon, Trash2Icon } from 'lucide-react'
+import toast from 'react-hot-toast'
+import { Link } from 'react-router'
 
 export default function NoteDetailPage() {
   const { id } = useParams() // get id from url
@@ -15,11 +17,15 @@ export default function NoteDetailPage() {
 
   async function updateNote() {
     try {
-      api.patch(`/notes/${id}`)
+      const res = await api.get(`/notes/${id}`)
+      console.log({ res })
+
+      setNote(res.data)
     } catch (error) {
       console.log(`Error updating note`, error)
+      toast.error('Failed to fetch the note')
     } finally {
-      // setLoading(false)
+      setLoading(false)
     }
   }
 
@@ -29,15 +35,30 @@ export default function NoteDetailPage() {
 
   if (loading) {
     return (
-      <div className="min-h-screen flex items-center justify-center">
+      <div className="min-h-screen flex items-center justify-center bg-base-200">
         <LoaderIcon className="animate-spin size-10" />
       </div>
     )
   }
 
   return (
-    <div className="min-h-screen">
-      <h1>Current Note ID: {id}</h1>
+    <div className="min-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8">
+        {/* Top section */}
+        <div className="flex justify-between align-center mb-6">
+          <Link
+            to={`/`}
+            className="btn btn-ghost">
+            <ArrowLeftIcon className="size-5" /> Back to Notes
+          </Link>
+
+          <button className="btn btn-error btn-outline">
+            <Trash2Icon className="size-5" /> Delete Note
+          </button>
+        </div>
+
+        {/* Form Section */}
+      </div>
     </div>
   )
 }
