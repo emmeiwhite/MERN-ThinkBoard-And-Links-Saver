734809269940a1b4e23cf23de82aaa293ffd2094
emmeiwhite <emmeiwhite@gmail.com>
2026-03-05 17:39:26 +0530

    chore: deployment settings --- (WIP)

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,9 +4,12 @@ import notesRouter from './routes/notesRoutes.js'
 import connectDB from './config/db.js'
 import dotenv from 'dotenv'
 import rateLimiter from './middleware/rateLimiter.js'
+import path from 'path'
 
 dotenv.config()
 
+const __dirname = path.resolve()
+console.log(__dirname)
 const app = express()
 
 // Allow multiple origins (dev + prod)
@@ -22,6 +25,15 @@ const PORT = process.env.PORT || 3000
 
 app.use(rateLimiter) // order matters, First ratelimit will check how many requests are made by the user and accordinly allow or restrict requests from the user.
 app.use('/api/v1/notes', notesRouter)
+
+// Serving static assets --- In our case, optimised React App within dist folder
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+// For routes other than /api/v1/note, we will serve our Optimised FE
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
+})
 
 // Connect to DB
 connectDB().then(() => {
