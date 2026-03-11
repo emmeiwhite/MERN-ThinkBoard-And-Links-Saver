8d22f1c4340e20f3e788e6b282bce2bfd4fa2c90
emmeiwhite <emmeiwhite@gmail.com>
2026-03-11 17:47:46 +0530

    chore: Logout controller

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -133,3 +133,23 @@ export const loginUser = async (req, res) => {
     })
   }
 }
+
+/**
+ LOGOUT 
+ */
+
+export const logoutUser = (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      return res.status(500).json({
+        message: 'Logout failed'
+      })
+    }
+
+    res.clearCookie('connect.sid')
+
+    res.json({
+      message: 'Logged out successfully'
+    })
+  })
+}
