a21f600cf37d580f640a88941fd4caec35e928b6
emmeiwhite <emmeiwhite@gmail.com>
2026-03-14 17:10:53 +0530

    chore: /me as protected route

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,7 +1,13 @@
 import express from 'express'
-import { registerUser, loginUser, logoutUser } from '../controllers/authController.js'
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getCurrentUser
+} from '../controllers/authController.js'
 const authRouter = express.Router()
 
+import { protectRoute } from '../middleware/authMiddleware.js'
 // Auth Routes
 
 // 1. POST /api/v1/auth/register
@@ -9,8 +15,8 @@ authRouter.post('/register', registerUser)
 authRouter.post('/login', loginUser)
 authRouter.delete('/logout', logoutUser)
 
-// 4. Get Current User
+// 4. Get Current User | Protected Route
 
-authRouter.get('/me')
+authRouter.get('/me', protectRoute, getCurrentUser)
 
 export default authRouter
