49ea857e8466a47861d41af8810113337b7b1804
emmeiwhite <emmeiwhite@gmail.com>
2026-03-15 19:58:49 +0530

    chore: When the app loads we ask the backend: Are we logged in?

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 // 1. Create Context (We are importing it as VSCode is not allowing multiple exports)
 import { AuthContext } from './AuthContext'
+import api from '../lib/axios'
 
 // 2. Create Provider --- which wraps our entire component tree and makes state available
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
 
   const login = user => {
     setUser(user)
@@ -20,7 +22,24 @@ export const AuthProvider = ({ children }) => {
   const contextValues = {
     user,
     login,
-    logout
+    logout,
+    loading
   }
+
+  const checkAuth = async () => {
+    try {
+      const res = await api.get('/auth/me')
+      setUser(res.data.user)
+    } catch (error) {
+      console.log(error)
+      setUser(null)
+    } finally {
+      setLoading(false)
+    }
+  }
+  // On page load, we'll check /auth/me (Is User logged in)
+  useEffect(() => {
+    checkAuth()
+  }, [])
   return <AuthContext.Provider value={contextValues}>{children}</AuthContext.Provider>
 }
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 // in production there is no localhost, so we have to make BASE_URL dynamic
 const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:3000/api/v1' : '/api/v1'
+
 const api = axios.create({
   baseURL: BASE_URL,
   withCredentials: true
