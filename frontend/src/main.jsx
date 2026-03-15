950f026d73b307b478946e2b8ac280cf2c9ec480
emmeiwhite <emmeiwhite@gmail.com>
2026-03-15 19:59:51 +0530

    chore: Wrap the App with Auth Provider --- every component can access auth state.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
 import { Toaster } from 'react-hot-toast'
+import { AuthProvider } from './context/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-    <Toaster />
-  </BrowserRouter>
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </AuthProvider>
 )
