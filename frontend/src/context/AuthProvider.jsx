7eb67d8e52906f8c3d510f12bdde8e183929a639
emmeiwhite <emmeiwhite@gmail.com>
2026-03-17 06:37:50 +0530

    chore: production version AuthProvider

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -4,18 +4,45 @@ import { useState, useEffect } from 'react'
 import { AuthContext } from './AuthContext'
 import api from '../lib/axios'
 
-// 2. Create Provider --- which wraps our entire component tree and makes state available
+// 2. Create Provider --- which wraps our entire component tree and makes global state available
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null)
   const [loading, setLoading] = useState(true)
 
-  const login = user => {
-    setUser(user)
+  const checkAuth = async () => {
+    try {
+      const res = await api.get('/auth/me')
+      console.log('Result:', res)
+      setUser(res.data.user)
+    } catch (error) {
+      console.log(error)
+      setUser(null)
+    } finally {
+      setLoading(false)
+    }
   }
 
-  const logout = () => {
-    setUser(null)
+  const login = async credentials => {
+    const res = await api.post('/auth/login', credentials)
+    setUser(res.data.user)
+
+    return res.data.user
   }
+
+  const logout = async () => {
+    try {
+      await api.delete('/auth/logout')
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setUser(null)
+    }
+  }
+
+  // On page load, we'll check /auth/me (Is User logged in)
+  useEffect(() => {
+    checkAuth()
+  }, [])
 
   // Context Object
 
@@ -26,21 +53,5 @@ export const AuthProvider = ({ children }) => {
     loading
   }
 
-  const checkAuth = async () => {
-    try {
-      const res = await api.get('/auth/me')
-      console.log('Result:', res)
-      // setUser(res.data.user)
-    } catch (error) {
-      console.log(error)
-      setUser(null)
-    } finally {
-      setLoading(false)
-    }
-  }
-  // On page load, we'll check /auth/me (Is User logged in)
-  useEffect(() => {
-    checkAuth()
-  }, [])
   return <AuthContext.Provider value={contextValues}>{children}</AuthContext.Provider>
 }
